e2cd82429038393ae38776be290a5e81b3b25a9c
Victor M. Varela <vmvarela@gmail.com>
2026-04-15 08:43:05 +0200

    chore: Delete packaging/chocolatey/tools directory

--- packaging/chocolatey/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$binary   = Join-Path $toolsDir 'sql-pipe.exe'
-
-if (Test-Path $binary) {
-    Remove-Item $binary -Force
-}
